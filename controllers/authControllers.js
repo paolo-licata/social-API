4afb643e15ed258baf29cdd797d86a48664a6ca6
paolo-licata <paolo_licata@outlook.com>
2025-02-28 17:14:36 +0100

    Created two more endpoints for user data update and user deletion, plus relevant routing

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,7 @@ const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+// Sign up a new user
 exports.registerUser = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -15,6 +16,7 @@ exports.registerUser = async (req, res) => {
     }
 }
 
+// Log in an existing user
 exports.loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -27,5 +29,41 @@ exports.loginUser = async (req, res) => {
         res.json({ token });
     } catch (error) {
         res.status(500).json({ message: "Login failed.", error });
+    }
+}
+
+// Update user details
+exports.updateUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const userId = req.user.id;
+
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: "User not found." });
+
+        // Update user details
+        if (username) user.username = username;
+        if(email) user.email = email;
+        if(password) user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        res.status(200).json({ message: "Account data updated successfully." });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update account data.", error });
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ nessage: "User not found"});
+
+        await User.findByIdAndDelete(userId);;
+
+        res.status(200).json({ message: "Account deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to delete account.", error });
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const { registerUser, loginUser } = require("../controllers/authControllers");
+const { registerUser, loginUser, updateUser, deleteUser } = require("../controllers/authControllers");
+const { verifyToken } = require("../middleware/auth");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/update", verifyToken, updateUser);
+router.delete("/delete", verifyToken, deleteUser);
 
 module.exports = router;
