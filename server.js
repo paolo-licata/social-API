9be48e7069283ad365e743f13a8aacb57c02b682
paolo-licata <paolo_licata@outlook.com>
2025-03-03 13:37:03 +0100

    Small changed for readability prior to deployment

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const postRoutes = require("./routes/post");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
@@ -25,6 +24,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
